db6fe2cdf90374c0838b7d290933195eb1de5ffb
Shuxin Lin <linshuhsin@gmail.com>
2015-02-15 02:26:08 +0000

    Added

---
+++ medium/Sqrt.java
@@ -0,0 +1,32 @@
+// Sqrt(x) 
+// https://oj.leetcode.com/problems/sqrtx/
+public class Solution {
+    public static int sqrt(int x) {
+        // count digits in binary repr of x.
+        int d = 0, j = x, i = 0;
+        while(j != 0){
+            i++;
+            if ((j&1)==1)
+                d = i;
+            j >>= 1;
+        }
+        
+        int candidDigits = d/2;
+        long min = Math.max(0, 1<<(candidDigits-1));
+        long max = 1<<(candidDigits+1);
+        
+        long lo = min, hi = max;
+        while(hi >= lo){
+            long med = (hi+lo)/2;
+            long medsq = med*med;
+
+            if (medsq == x) return (int)med;
+            else if (medsq < x) lo = med+1;
+            else if (medsq > x) hi = med-1;
+        }
+        
+        // if result not found, use one termination point 
+        
+        return (int) ((hi*hi > x)? hi - 1 : hi);
+    }
+}
