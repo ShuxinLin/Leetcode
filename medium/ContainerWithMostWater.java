0ba1e8109ae196c5056c91c4ee884a9201fb847a
Shuxin Lin <linshuhsin@gmail.com>
2015-03-16 22:28:31 +0000

    Added

---
+++ medium/ContainerWithMostWater.java
@@ -0,0 +1,34 @@
+// Container With Most Water
+// https://leetcode.com/problems/container-with-most-water/
+public class Solution {
+    public int maxArea(int[] height) {
+        int maxArea = 0;
+        
+        int l = 0, r = height.length-1;
+        while(r >= l){
+            int area = (r-l)*Math.min(height[r], height[l]);
+            if (area > maxArea) maxArea = area;
+            
+            if (height[l] < height[r])
+                l++;
+            else
+                r--;
+        }
+        return maxArea;
+    }
+    
+    // O(N^2) didn't pass the large input 10000..1
+    public int bruteForce_maxArea(int[] height) {
+        int maxArea = 0;
+        
+        for(int i = 0; i < height.length; i++){
+            for(int j = i+1; j < height.length; j++){
+                int area = (j-i)*Math.min(height[i], height[j]);
+                if (area > maxArea) maxArea = area;
+            }
+        }
+        return maxArea;
+    }
+    
+    
+}
